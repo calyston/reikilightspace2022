23dfc01599e22525877bdf2935d23a84cff7383b
Christine <calyston.14@gmail.com>
2022-03-18 12:18:02 +0000

    feat: contact page created and styled

--- page-contact.php
+++ page-contact.php
@@ -9,7 +9,7 @@ get_header(); ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 		
-  <section class="contact-page">
+  <section class="page-container contact-page">
     <h2 class="page-title"><?php the_title(); ?></h2>
     <div class="contact-p"><?php the_content(); ?></div>
     <section class="contact-content">
@@ -36,8 +36,8 @@ get_header(); ?>
         </form>
       </div>
       <div class="contact-address">
-        <span>Reiki Light Space</span>
-        <?php dynamic_sidebar('contact-info'); ?>
+        <h4>Reiki Light Space</h4>
+        <p>Catford, London, United Kingdom</p>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19895.263580574756!2d-0.03188681872088049!3d51.44148439589588!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48760217687bbabf%3A0xaa75a3ab26a3bb67!2sCatford%2C%20London!5e0!3m2!1sen!2suk!4v1624380674017!5m2!1sen!2suk" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
       </div> 
     </section>
